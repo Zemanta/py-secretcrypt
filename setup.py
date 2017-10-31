2ef46cceef6f65e9f3e5b561d84a28596ca90d95
Nejc Saje <nejc@saje.info>
2017-10-31 07:06:12 +0100

    Back to development: 1.0.1

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ history = history.replace(".. :changelog:", "")
 setup(
     name='secretcrypt',
     packages=['secretcrypt'],
-    version='1.0.0',
+    version='1.0.1.dev0',
     description='Encrypt project secrets',
     long_description=readme + '\n\n' + history,
     author='Nejc Saje, Zemanta',
